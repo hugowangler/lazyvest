58e6563c1593dda8b24e9491ac6db54e07bfed09
Hugo Wangler <hugowangler@gmail.com>
2022-02-19 20:02:53 +0100

    init poetry project

---
+++ portfolio/portfolio/portfolio.py
@@ -0,0 +1,18 @@
+""" Module containing the portfolio optimization """
+
+
+def main():
+    """lol"""
+    print(
+        "fat lol loaksjdk"
+        + "hasjdhajkshd"
+        + "ajsdhajkshdja"
+        + "hasjdhajkshdkajshd"
+    )
+
+    hej = "tja"
+    return hej
+
+
+if __name__ == "__main__":
+    main()
